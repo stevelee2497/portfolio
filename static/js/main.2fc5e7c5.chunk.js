6648935f10748a17adec2b9172b241a78c2d6c7b
Quoc Tran <quoc.tran@saigontechnology.com>
2021-04-25 15:01:22 +0700

    Updates

---
+++ static/js/main.2fc5e7c5.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),i=n.n(r),s=(n(8),n(9),n(0));var l=function(){return Object(s.jsx)("div",{className:"p-4",children:"hello world"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(l,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.2fc5e7c5.chunk.js.map
